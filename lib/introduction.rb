2a2b29a5377551b40c135931d624ad9b5c11175a
Jean-Baptiste Garway-Templeman <jbgarwayt@gmail.com>
2019-07-31 14:04:13 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,6 +6,14 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
+<<<<<<< HEAD
 def introduction_with_language_optional(name, language="Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+=======
+def introduction_with_language_optional(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language_optional(name, ruby)
+>>>>>>> 7090d0544fc4ce90ed987cdd83c76842aeaf3542
